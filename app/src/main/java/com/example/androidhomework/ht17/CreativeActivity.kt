9a0fae7a695c0923beccc2b023ee059a4c06199c
Ivan Mazko <mazkovania@gmail.com>
2024-10-21 19:01:50 +0300

    feat: made ht17

--- app/src/main/java/com/example/androidhomework/ht17/CreativeActivity.kt
+++ app/src/main/java/com/example/androidhomework/ht17/CreativeActivity.kt
@@ -2,6 +2,7 @@ package com.example.androidhomework.ht17
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,5 +15,9 @@ class CreativeActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_creative)
         val button = findViewById<Button>(R.id.Translate_button)
+        val text = findViewById<TextView>(R.id.tvTextToTranslate)
+        button.setOnClickListener {
+            text.text = resources.getString(R.string.myText)
+        }
     }
 }
