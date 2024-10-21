3c107c7581e8fe93617abfb1d9c03e075338d7e0
Ivan Mazko <mazkovania@gmail.com>
2024-10-21 17:41:39 +0300

    feat: made a part of hometask

--- app/src/main/java/com/example/androidhomework/ht17/CreativeActivity.kt
+++ app/src/main/java/com/example/androidhomework/ht17/CreativeActivity.kt
@@ -1,6 +1,7 @@
 package com.example.androidhomework.ht17
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,10 +13,6 @@ class CreativeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_creative)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        val button = findViewById<Button>(R.id.Translate_button)
     }
 }
