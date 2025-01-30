2f1191c924f7f615ebf3bfbbe8e49684b0f38b20
Ivan Mazko <mazkovania@gmail.com>
2025-01-30 15:28:54 +0300

    feat: +1 file committed

--- app/src/main/java/com/example/androidhomework/presentation/view/fragments/MainFragment.kt
+++ app/src/main/java/com/example/androidhomework/presentation/view/fragments/MainFragment.kt
@@ -27,7 +27,7 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainFragment : Fragment() {
 
-    private val viewModel: MainFragmentViewModel? by viewModel()
+    private val viewModel: MainFragmentViewModel by viewModel()
     private val listOfNotes: ArrayList<Note> = ArrayList()
     private var adapter: Adapter? = null
 
